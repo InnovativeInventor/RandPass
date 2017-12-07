6e556969bdf48a6949df66e69159d32c391c3d95
Inventor <theinnovativeinventor@gmail.com>
2017-12-06 22:05:05 -0500

    Updates and fixes

--- password.py
+++ password.py
@@ -16,32 +16,24 @@ def word_simple():
     word = lines[rand]
     return word
 
-def complex():
-    num = secrets.randbelow(3)
-    randnums = secrets.randbelow(10)
+def complex(length=2,num_length=2):
+    listpass = []
+    num = secrets.randbelow(length)
+    randnums = secrets.randbelow(10**num_length)
     randnum = str(randnums)
 
-    word1=word_complex()
-    word2=word_complex()
-    parsed_word1 = word1.rstrip()
-    parsed_word2 = word2.rstrip()
-    # print ("Word1: " + word1)
-    # print ("Word1: " + word2)
-    if num == 0:
-        pass_text = parsed_word1 + parsed_word2 + randnum
-
-    if num == 1:
-        pass_text = parsed_word1 + randnum + parsed_word2
-
-    if num == 2:
-        pass_text = randnum + parsed_word1 + parsed_word2
-
-    return(pass_text)
+    count = 0
+    while count < length:
+        listpass.append(word_complex().rstrip())
+        count += 1
+    listpass.insert(num-1, randnum)
+    gen_pass = undo_list(listpass)
+    return(gen_pass)
 
 def simple(length=2,num_length=2):
     listpass = []
     num = secrets.randbelow(length)
-    randnums = secrets.randbelow(num)
+    randnums = secrets.randbelow(10**num_length)
     randnum = str(randnums)
 
     count = 0
@@ -57,15 +49,51 @@ def undo_list(input):
     undo_list = ''.join(input)
     return undo_list
 
-# print(complex())
-length=2
-num_length=2
-print (sys.argv[1])
-if sys.argv:
-    length = int(sys.argv[1])
-    num_length = int(sys.argv[2])
+def help():
+    print()
+    print("Usage:")
+    print(sys.argv[0] + " <number of words> <digits of random number>")
+    print()
+    print("Contributions are welcome!")
+    sys.exit(1)
 
+def cli_args(length=2,num_length=2):
+    # Command-line Args
+    if len(sys.argv) == 3:
+        length = int(sys.argv[1])
+        num_length = int(sys.argv[2])
+
+    # Detect simple vs complex vs full
+    elif str(sys.argv[1]) == 'complex' or str(sys.argv[1]) == 'c' or str(sys.argv[1]) == '--complex' or str(sys.argv[1]) == '-c':
+        gen=complex(length,num_length)
+        print ("complex")
+
+    # Detect help
+    elif str(sys.argv[1]) == 'h' or str(sys.argv[1]) == 'help' or str(sys.argv[1]) == '-h' or str(sys.argv[1]) == '--help':
+        help()
+
+    elif len(sys.argv) == 2:
+        print("Error: Incorrect number of arguments provided.")
+        print("If you are going to pass options, please see the output of -help below.")
+        help()
+    return gen
+
+length=int(2)
+num_length=int(2)
+
+try:
+    len(sys.argv)
+    args="true"
+
+except:
+    args="false"
+
+if args == "true":
+    gen = cli_args()
 else:
-    exit
-print(simple(length,num_length))
-print(sys.argv)
+    gen=simple(length,num_length)
+
+print(gen)
+
+# ## todo
+# Errors: ./password.py complex 3
